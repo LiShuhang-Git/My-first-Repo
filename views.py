3e150ed4214e0af40e24215ec0ff8d64fee7f45e
LiShuhang-Git <Shuhang.l@outlook.com>
2019-06-12 15:12:22 -0400

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 #####在这里写上要开发的内容，比如：
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
 
 def index(request):
 	return HttpResponse('index')
+
+def login(request):
+	return redirect('/index')
+
 
